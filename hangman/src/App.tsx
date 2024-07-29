695c6e97ed2fef22bca877bea0b71e9dc42fba81
DaniM0613 <wwh.rj.92@gmail.com>
2024-07-29 15:52:12 -0600

    creating a game for practicing TS and React

---
+++ hangman/src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+import words from './wordList.json'
+
+function App() {
+
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  })
+  
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+  console.log(wordToGuess)
+  return (
+    <h1>
+      Hello!
+    </h1>
+  )
+}
+
+export default App
